e9d6f14b0f771dec7f6f8945d72533df37a17433
LeAries <learies@ya.ru>
2023-08-08 21:58:43 +0300

    feat: Добавлен AbstractUser

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -97,3 +99,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
